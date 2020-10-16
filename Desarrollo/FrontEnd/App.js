dbe44f9ecf2a52203ea81e81a853c700e23db0e1
BFCalderon <bfcalderon47@misena.edu.co>
2020-10-16 14:04:14 -0500

    Servicios implementados. Get. Put. Post. Insert

--- Desarrollo/FrontEnd/App.js
+++ Desarrollo/FrontEnd/App.js
@@ -1,16 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Asd from '../backend/index'
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 export default function App() {
   return (
--- Desarrollo/backend/index.js
+++ Desarrollo/backend/index.js
@@ -31,6 +31,15 @@ app.get("/", (req, res) => {
   console.log("Bienbenido al backend");
 });
 
+
+app.listen(port, () => {
+  console.log(`Ejemplo escuchando http://${port}`);
+});
+
+
+/**
+ * Consulta toda los elementos de toda la tabla
+ */
 app.route("/rol").get((req, res) => {
   console.log("Get de Usuarios");
   const query = db.query('select * from rol', (error, resultado) => {
@@ -47,15 +56,9 @@ app.route("/rol").get((req, res) => {
   })
 });
 
-app.route("/user/:id").get((req, res) => {
-  const idUser = req.params.id;
-  console.log(idUser);
-  res.json({idUser: idUser})
-});
-
-app.route("/user").post((req, res) => {
-  console.log("Post de usuario");
-  const query = db.query(`insert into rol(id, nombre, descripcion) values (4,"Fabian Bastilla", "Vendedor de Correas")`, (error, resultado) => {
+app.route("/articulo").get((req, res) => {
+  console.log("Get de Usuarios");
+  const query = db.query('select * from articulo', (error, resultado) => {
     try{
       if(error){
         throw error;
@@ -69,6 +72,236 @@ app.route("/user").post((req, res) => {
   })
 });
 
-app.listen(port, () => {
-  console.log(`Ejemplo escuchando http://${port}`);
+app.route("/comerciante").get((req, res) => {
+  console.log("Get de Usuarios");
+  const query = db.query('select * from comerciante', (error, resultado) => {
+    try{
+      if(error){
+        throw error;
+      }else{
+        console.log(resultado)
+        res.json({resultado: resultado})
+      }
+    }catch(error){
+        res.json({error: error.message})
+    }
+  })
+});
+
+app.route("/venta").get((req, res) => {
+  console.log("Get de Usuarios");
+  const query = db.query('select * from venta', (error, resultado) => {
+    try{
+      if(error){
+        throw error;
+      }else{
+        console.log(resultado)
+        res.json({resultado: resultado})
+      }
+    }catch(error){
+        res.json({error: error.message})
+    }
+  })
+});
+
+/**
+ * Inserta datos en las tablas
+ */
+app.route("/articulo").post((req, res) => {
+  console.log("Post de articulo");
+  const query = db.query(`insert into articulo(id, codigo, nombre, precio, stock, descripcion) values (1, 1111, "Pantalon", 35000, 35, "Pantalon para caballero en drol")`, (error, resultado) => {
+    try{
+      if(error){
+        throw error;
+      }else{
+        console.log(resultado)
+        res.json({resultado: resultado})
+      }
+    }catch(error){
+        res.json({error: error.message})
+    }
+  })
+});
+
+app.route("/comerciante").post((req, res) => {
+  console.log("Post de comerciante");
+  const query = db.query(`insert into comerciante(id, nombre_propietario, nombre_marca, 	direccion_comercial, genero_ropa, tipo_ropa, categoria_ropa, rol_id) values (1,"Fabian Bastilla", "BFCalderon", "Cll 1#58-30", "Unisex", "Pantalon/Camisa", "Juvenil", 1)`, (error, resultado) => {
+    try{
+      if(error){
+        throw error;
+      }else{
+        console.log(resultado)
+        res.json({resultado: resultado})
+      }
+    }catch(error){
+        res.json({error: error.message})
+    }
+  })
+});
+
+app.route("/rol").post((req, res) => {
+  console.log("Post de rol");
+  const query = db.query(`insert into rol(id, nombre, descripcion) values (1,"Fabian Bastilla", "Vendedor de Correas")`, (error, resultado) => {
+    try{
+      if(error){
+        throw error;
+      }else{
+        console.log(resultado)
+        res.json({resultado: resultado})
+      }
+    }catch(error){
+        res.json({error: error.message})
+    }
+  })
+});
+
+app.route("/venta").post((req, res) => {
+  console.log("Post de venta");
+  const query = db.query(`insert into venta(id, tipo_comprobante, fecha, valor, comerciante_id, comerciante_rol, articulo_id) values (1,"Factura", "2020/03/10", 150000, 1, 1 ,1)`, (error, resultado) => {
+    try{
+      if(error){
+        throw error;
+      }else{
+        console.log(resultado)
+        res.json({resultado: resultado})
+      }
+    }catch(error){
+        res.json({error: error.message})
+    }
+  })
+});
+
+
+/**
+ * Trae información de las tablas filtradas por el id
+ */
+app.route("/rol/:id").get((req, res) => {
+  console.log("Lectura de un dato en rol");
+  const query = db.query(`select * from rol where id = ${req.params.id}`, (error, resultado) => {
+    try{
+      if(error){
+        throw error;
+      }else{
+        console.log(resultado)
+        res.json({resultado: resultado})
+      }
+    }catch(error){
+        res.json({error: error.message})
+    }
+  })
+});
+
+app.route("/comerciante/:id").get((req, res) => {
+  console.log("Lectura de un dato en comerciante");
+  const query = db.query(`select * from comerciante where id = ${req.params.id}`, (error, resultado) => {
+    try{
+      if(error){
+        throw error;
+      }else{
+        console.log(resultado)
+        res.json({resultado: resultado})
+      }
+    }catch(error){
+        res.json({error: error.message})
+    }
+  })
+});
+
+app.route("/venta/:id").get((req, res) => {
+  console.log("Lectura de un dato en venta");
+  const query = db.query(`select * from venta where id = ${req.params.id}`, (error, resultado) => {
+    try{
+      if(error){
+        throw error;
+      }else{
+        console.log(resultado)
+        res.json({resultado: resultado})
+      }
+    }catch(error){
+        res.json({error: error.message})
+    }
+  })
+});
+
+app.route("/articulo/:id").get((req, res) => {
+  console.log("Lectura de un dato en articulo");
+  const query = db.query(`select * from articulo where id = ${req.params.id}`, (error, resultado) => {
+    try{
+      if(error){
+        throw error;
+      }else{
+        console.log(resultado)
+        res.json({resultado: resultado})
+      }
+    }catch(error){
+        res.json({error: error.message})
+    }
+  })
+});
+
+/**
+ * Actualiza los datos en las tablas
+ */
+app.route("/articulo/:id").put((req, res) => {
+  console.log("Post de articulo");
+  const query = db.query(`Update articulo Set codigo = 1112, nombre = "Camisa", precio = 30000, stock = 30, descripcion = "Camisa para caballero" Where id = 1`, (error, resultado) => {
+    try{
+      if(error){
+        throw error;
+      }else{
+        console.log(resultado)
+        res.json({resultado: resultado})
+      }
+    }catch(error){
+        res.json({error: error.message})
+    }
+  })
+});
+
+app.route("/comerciante/:id").put((req, res) => {
+  console.log("Post de comerciante");
+  const query = db.query(`Update comerciante Set nombre_propietario = "Fabian Bastilla", nombre_marca = "Electronica BC", direccion_comercial = "Cra 44#19-64", genero_ropa = "masculino", rol_id = 1 Where id = 1`, (error, resultado) => {
+    try{
+      if(error){
+        throw error;
+      }else{
+        console.log(resultado)
+        res.json({resultado: resultado})
+      }
+    }catch(error){
+        res.json({error: error.message})
+    }
+  })
+});
+
+app.route("/rol/:id").put((req, res) => {
+  console.log("Post de rol");
+  const query = db.query(`Update rol Set nombre = "Elsa Bastilla", descripcion = "Vendedor de camisas" Where id = 1`, (error, resultado) => {
+    try{
+      if(error){
+        throw error;
+      }else{
+        console.log(resultado)
+        res.json({resultado: resultado})
+      }
+    }catch(error){
+        res.json({error: error.message})
+    }
+  })
+});
+
+app.route("/venta/:id").put((req, res) => {
+  console.log("Post de venta");
+  const query = db.query(`Update venta Set tipo_comprobante = "Factura Electrónica", fecha = "2020/10/12", comerciante_id = 1 Where id = 1`, (error, resultado) => {
+    try{
+      if(error){
+        throw error;
+      }else{
+        console.log(resultado)
+        res.json({resultado: resultado})
+      }
+    }catch(error){
+        res.json({error: error.message})
+    }
+  })
 });
